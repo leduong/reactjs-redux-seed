07ee735f04c7f4437af2d48c996413c67bc0d62f
Le Duong <me@leduong.com>
2018-10-16 21:05:08 +0700

    add eslint

--- server/express/middleware.js
+++ server/express/middleware.js
@@ -4,22 +4,22 @@ import Config from './config';
 const { secretKey } = Config;
 
 const authenticate = (req, res, next) => {
-	const token = req.headers.authorization || '';
-	jsonwebtoken.verify(token, secretKey, (error, decoded) => {
-		if (error) {
-			next({ error: 'token varified failed' });
-		} else {
-			const { expiredAt } = decoded;
-			if (expiredAt > new Date().getTime()) {
-				next();
-			} else {
-				next({ error: 'token expired' });
-			}
-		}
-	});
+  const token = req.headers.authorization || '';
+  jsonwebtoken.verify(token, secretKey, (error, decoded) => {
+    if (error) {
+      next({ error: 'token varified failed' });
+    } else {
+      const { expiredAt } = decoded;
+      if (expiredAt > new Date().getTime()) {
+        next();
+      } else {
+        next({ error: 'token expired' });
+      }
+    }
+  });
 };
 
-const authError = (err, req, res, next) => {
-	res.json(err);
+const authError = (err, req, res) => {
+  res.json(err);
 };
 export { authenticate, authError };
--- server/express/server.js
+++ server/express/server.js
@@ -9,53 +9,53 @@ const { port, secretKey, expiredAfter } = Config;
 const app = express();
 
 function doesUserExists(username, password) {
-	const user = {
-		id: 1,
-		username: 'demo@gmail.com',
-		password: 'demodemo',
-	};
-	if (username === user.username && password === user.password) {
-		return true;
-	}
-	return false;
+  const user = {
+    id: 1,
+    username: 'demo@gmail.com',
+    password: 'demodemo',
+  };
+  if (username === user.username && password === user.password) {
+    return true;
+  }
+  return false;
 }
 
 app
-	.use(bodyParser.urlencoded({ extended: true }))
-	.use(bodyParser.json())
-	.use(cors());
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(bodyParser.json())
+  .use(cors());
 
 app.get('/', (req, res) => {
-	res.json({ status: 'OK' });
+  res.json({ status: 'OK' });
 });
 
 app.post('/api/login', (req, res) => {
-	const { username, password } = req.body;
-	const response = {};
-	// You can use DB checking here
+  const { username, password } = req.body;
+  const response = {};
+  // You can use DB checking here
 
-	if (doesUserExists(username, password)) {
-		response.token = jsonwebtoken.sign(
-			{
-				expiredAt: new Date().getTime() + expiredAfter,
-				username,
-				id: 1,
-			},
-			secretKey
-		);
-	} else {
-		response.error = 'Not found';
-	}
-	res.json(response);
+  if (doesUserExists(username, password)) {
+    response.token = jsonwebtoken.sign(
+      {
+        expiredAt: new Date().getTime() + expiredAfter,
+        username,
+        id: 1,
+      },
+      secretKey
+    );
+  } else {
+    response.error = 'Not found';
+  }
+  res.json(response);
 });
 app.use('/api/secret', [authenticate, authError]);
 app.post('/api/secret/test', (req, res) => {
-	res.json({
-		status: 200,
-		message: 'succcesful',
-	});
+  res.json({
+    status: 200,
+    message: 'succcesful',
+  });
 });
 
 app.listen(port, () => {
-	console.log('reactdds JWT login ' + port);
+  console.log('reactdds JWT login ' + port);
 });
